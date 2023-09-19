d3686b3aa37ee12669a1ea64b5b3a1014d49425a
kumailraza404 <kumailraza404@gmail.com>
2023-09-19 15:28:44 +0000

    minor changes to tokens route

--- index.ts
+++ index.ts
@@ -58,27 +58,56 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/tokens', async (req, res) => {
-  const page = req.query?.page || '1';
+  // const page = req.query?.page || '1';
 
   // Convert the structure into an array of key-value pairs
-  const coinsArray: [string, CoinInfo][] = Object.entries(TOKENS);
+  // const coinsArray: [string, CoinInfo][] = Object.entries(TOKENS);
 
   // Slice the first 10 items
-  const first10Items: [string, CoinInfo][] = coinsArray.slice(
-    0,
-    parseInt(page as string) * 10,
-  );
+  // const first10Items: [string, CoinInfo][] = coinsArray.slice(
+  //   0,
+  //   parseInt(page as string) * 10,
+  // );
 
-  // If you want to convert it back to an object
-  const result: Record<string, CoinInfo> = first10Items.reduce(
-    (acc, [key, value]) => {
-      acc[value.address] = value;
-      return acc;
-    },
-    {} as Record<string, any>,
-  );
+  // // If you want to convert it back to an object
+  // const result: Record<string, CoinInfo> = first10Items.reduce(
+  //   (acc, [key, value]) => {
+  //     acc[value.address] = value;
+  //     return acc;
+  //   },
+  //   {} as Record<string, any>,
+  // );
 
-  res.send(result);
+  // Define the pagination parameters
+
+  const page = req.query?.page ? parseInt(req.query.page as string) : 1; // Get the requested page, default to 1 if not provided
+  const limit = req.query?.limit ? parseInt(req.query.limit as string) : 10; // Number of items per page, default to 10 if not provided
+
+  // Calculate the start and end indexes for the current page
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  //All Tokens from the database - json
+  const allTokens: [string, CoinInfo][] = Object.entries(TOKENS);
+
+
+  // Slice the array to get the tokens for the current page
+  const tokensOnPage = allTokens.slice(startIndex, endIndex);
+
+  // Check if there are more pages
+  const hasMore = endIndex < allTokens.length;
+
+
+
+
+  res.send({
+    tokens: tokensOnPage,
+    currentPage: page,
+    limit: limit,
+    totalPages: Math.ceil(allTokens.length / limit),
+    totalTokens: allTokens.length,
+    hasMore: hasMore
+  });
 });
 
 app.get('/tokens/search', async (req, res) => {
